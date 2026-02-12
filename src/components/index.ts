f73c110f482d2df7e0a12551dbdfa7a0fb068630
Guido Mantegna <Guido>
2026-02-12 17:16:57 -0300

    Reorder components & dashboard's layout

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import TableRow from "./TableRow";
+import Toolbar from "./Toolbar";
+import Pagination from "./Pagination";
+import DetailSection from "./DetailSection";
+import Sidebar from "./Sidebar";
+import StatCard from "./StatCard";
+import Header from "./Header";
+
+export { TableRow, Toolbar, Pagination, DetailSection, Sidebar, StatCard, Header };
